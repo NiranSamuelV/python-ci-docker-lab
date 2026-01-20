2ac3eb026b1a36d8dab5be71eeb3dcac5c2c9d83
Mr. Niran Samuel V <urk23cs1014@KARUNYA.EDU>
2026-01-20 16:24:14 +0530

    init lab

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,4 @@
-def add(a,b):
-    return a+b
-
-if __name__=="__main__":
-    print("Hello from Python CI Lab!")
-    print("2+3=",add(2,3))
+from app import add
+def test_add():
+    assert add(2,3) == 5
+    assert add(-1,1) == 0
